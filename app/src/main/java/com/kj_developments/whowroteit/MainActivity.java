f60f385468b3aee77e0fe65b2e9208b3a1a63069
Kiran-Ahir <69784812+Kiran-Ahir@users.noreply.github.com>
2020-11-29 16:08:15 +0530

    Updated Android studio

--- app/src/main/java/com/kj_developments/whowroteit/MainActivity.java
+++ app/src/main/java/com/kj_developments/whowroteit/MainActivity.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.ref.WeakReference;
